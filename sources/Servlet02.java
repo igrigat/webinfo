e2dac01e513b7e9536216d3503af4790c487fd8c
Ingo Grigat <ingo@grigathome.de>
2011-05-22 22:56:29 +0200

    package changed

--- sources/Servlet02.java
+++ sources/Servlet02.java
@@ -4,7 +4,7 @@
   Fragestellung : Servlet-Klasse, Ausgabe einiger Methode von Anfrage-Objekt.
   Version       : 10.02.2011
 */
-package u00;
+package u08;
 
 import java.io.*;
 import javax.servlet.*;
